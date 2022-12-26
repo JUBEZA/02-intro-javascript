307b68c35fa509b0daf1d7e11265dea92e55d540
Juan Bezanilla <juanbezanillaz@gmail.com>
2022-12-26 20:10:18 -0300

    25 Operation conditional ternario

---
+++ src/bases/12-operador-condicional-ternario.js
@@ -0,0 +1,23 @@
+
+
+const activo = true;
+
+// let mensaje = '';
+
+// if ( activo ) {
+//   mensaje = 'Activo';
+// } else {
+//   mensaje = 'Inactivo';
+// }
+
+
+
+// const mensaje = ( activo ) ? 'Activo' : 'Inactivo';
+
+
+// const mensaje = ( !activo ) ? 'Activo' : 'Null';
+
+
+const mensaje = (activo === true) && 'Activo'
+
+console.log(mensaje);
